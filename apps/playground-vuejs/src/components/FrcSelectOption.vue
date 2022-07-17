44752f4a545a78196e03b00165db1dde8f3e478e
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-07-17 13:26:48 -0700

    Rename counter to router

---
+++ apps/playground-vuejs/src/components/FrcSelectOption.vue
@@ -0,0 +1,19 @@
+<template>
+  <select v-model="selectedIngredient">
+    <option disabled :value="{}">Please select one</option>
+    <option v-for="option in ingredientOptions" :value="option">
+      {{ option.supplyName }}
+    </option>
+  </select>
+  <button @click="onClickNewIngredient">
+    Add Ingredient - {{ selectedIngredient.supplyName }}
+  </button>
+</template>
+<script setup lang="ts">
+type Props = {
+  selectedIngredient: any;
+  ingredientOptions: any;
+};
+
+defineProps<Props>();
+</script>
--- apps/playground-vuejs/src/main.js
+++ apps/playground-vuejs/src/main.js
@@ -1,9 +1,5 @@
 import App from "@/App.vue";
-import counter from "@/router";
+import router from "@/router";
 import { createApp } from "vue";
 
-const app = createApp(App);
-
-app.use(counter);
-
-app.mount("#app");
+createApp(App).use(router).mount("#app");
