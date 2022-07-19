d521354446fa8f0629c8ce15b2b2b53860b104dc
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-07-18 20:21:09 -0700

    Fix env var name

--- apps/fernwood-costing/vue.config.js
+++ apps/fernwood-costing/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.COST_APP_PUBLIC_PATH ?? "/",
+  publicPath: process.env.COSTING_APP_PUBLIC_PATH ?? "/",
 });
