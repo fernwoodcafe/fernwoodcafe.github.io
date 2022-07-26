ad2fdba3cb6e0c26ec21f4c9a41226ad7c00b924
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-07-25 21:24:27 -0700

    Successfully query graph.microsoft.com/v1.0/me/drive/root/children

--- apps/playground-ms-identity-javascript/app/graph.js
+++ apps/playground-ms-identity-javascript/app/graph.js
@@ -1,22 +1,29 @@
-/** 
+/**
  * Helper function to call MS Graph API endpoint
  * using the authorization bearer token scheme
-*/
+ */
 function callMSGraph(endpoint, token, callback) {
-    const headers = new Headers();
-    const bearer = `Bearer ${token}`;
+  const headers = new Headers();
+  const bearer = `Bearer ${token}`;
 
-    headers.append("Authorization", bearer);
+  headers.append("Authorization", bearer);
 
-    const options = {
-        method: "GET",
-        headers: headers
-    };
+  const options = {
+    method: "GET",
+    headers: headers,
+  };
 
-    console.log('request made to Graph API at: ' + new Date().toString());
+  console.log(
+    "request made to Graph API at: " +
+      new Date().toString() +
+      " to " +
+      endpoint +
+      " with token " +
+      bearer
+  );
 
-    fetch(endpoint, options)
-        .then(response => response.json())
-        .then(response => callback(response, endpoint))
-        .catch(error => console.log(error));
+  fetch(endpoint, options)
+    .then((response) => response.json())
+    .then((response) => callback(response, endpoint))
+    .catch((error) => console.log(error));
 }
