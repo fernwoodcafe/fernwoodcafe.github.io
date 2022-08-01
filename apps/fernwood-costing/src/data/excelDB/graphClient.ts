a6b645c16fc4effb103f7a3e23a1ac37f0dba4c1
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-08-01 14:15:28 -0700

    Refactor for SPOT

--- apps/fernwood-costing/src/data/excelDB/graphClient.ts
+++ apps/fernwood-costing/src/data/excelDB/graphClient.ts
@@ -2,70 +2,46 @@ import type { DomainEvent } from "@/types/CafeDomain";
 import { getTokenRedirect, signIn } from "./auth";
 import { graphConfig, loginRequest } from "./msalConfig";
 
-export const httpPost = (endpoint, body, accessToken, callback) => {
-  const headers = new Headers();
-  const bearer = `Bearer ${accessToken}`;
-  headers.append("Authorization", bearer);
-  const options = {
+const fetchJson = async (request: Request) => {
+  try {
+    const tokenResponse = await getTokenRedirect(loginRequest);
+    const bearer = `Bearer ${tokenResponse.accessToken}`;
+    request.headers.append("Authorization", bearer);
+    return fetch(request).then((response) => response.json());
+  } catch {
+    await signIn();
+  }
+};
+
+export const httpPost = (endpoint, body) => {
+  const request = new Request(endpoint, {
     method: "POST",
-    headers: headers,
+    headers: new Headers(),
     body: JSON.stringify(body),
-  };
+  });
 
-  fetch(endpoint, options)
-    .then((response) => response.json())
-    .then((response) => callback(response, endpoint))
-    .catch((error) => console.log(error));
+  return fetchJson(request);
 };
 
-export const httpGet = (endpoint, accessToken, callback) => {
-  const headers = new Headers();
-  const bearer = `Bearer ${accessToken}`;
-  headers.append("Authorization", bearer);
-  const options = {
+export const httpGet = (endpoint) => {
+  const request = new Request(endpoint, {
     method: "GET",
-    headers: headers,
-  };
+    headers: new Headers(),
+  });
 
-  fetch(endpoint, options)
-    .then((response) => response.json())
-    .then((response) => callback(response, endpoint))
-    .catch((error) => console.log(error));
-};
-
-export const seeProfile = async () => {
-  const response = await getTokenRedirect(loginRequest);
-  httpGet(graphConfig.graphMeEndpoint, response.accessToken, (data, endpoint) =>
-    console.log("msGraphResponse", endpoint, data)
-  );
-};
-
-export const fetchExcelData = async () => {
-  const response = await getTokenRedirect(loginRequest);
-
-  httpGet(
-    graphConfig.graphDriveEndpoint,
-    response.accessToken,
-    (data, endpoint) => console.log("msGraphResponse", endpoint, data)
-  );
+  return fetchJson(request);
 };
 
 export const $readMany = async (): Promise<DomainEvent[]> => {
   const workbookId = "01WODPJHNUFIDRETOZIRDL77KL5NJ7RRVS";
-  try {
-    const tokenResponse = await getTokenRedirect(loginRequest);
-    httpPost(
-      // `${graphConfig.graphDriveEndpoint}/root/children/`,
-      `${graphConfig.graphDriveEndpoint}/items/${workbookId}/workbook/createSession`,
-      {
-        persistChanges: true,
-      },
-      tokenResponse.accessToken,
-      (data, endpoint) => console.log("msGraphResponse", endpoint, data)
-    );
+  const json = await httpPost(
+    `${graphConfig.graphDriveEndpoint}/items/${workbookId}/workbook/createSession`,
+    {
+      persistChanges: true,
+    }
+  );
 
-    return [];
-  } catch {
-    await signIn();
-  }
+  console.log(json);
+
+  return [];
 };
