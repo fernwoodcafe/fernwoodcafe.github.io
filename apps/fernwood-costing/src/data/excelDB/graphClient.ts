a4dc3a58205e8c369af18245b639a577ad1f7b23
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-08-01 16:14:33 -0700

    wip: Authentication on page load

--- apps/fernwood-costing/src/data/excelDB/graphClient.ts
+++ apps/fernwood-costing/src/data/excelDB/graphClient.ts
@@ -6,7 +6,17 @@ import {
 } from "@microsoft/microsoft-graph-client/authProviders/authCodeMsalBrowser";
 import { msalConfig } from "./msalConfig";
 
+console.log("foo");
+
 const publicClientApplication = new PublicClientApplication(msalConfig);
+
+// Redirect: once login is successful and redirects with tokens, call Graph API
+const response = await publicClientApplication.handleRedirectPromise();
+
+console.log(response);
+
+publicClientApplication.setActiveAccount(response.account);
+
 const options: AuthCodeMSALBrowserAuthenticationProviderOptions = {
   // For account see https://github.com/microsoftgraph/msgraph-sdk-javascript/issues/505
   account: null,
@@ -18,6 +28,8 @@ const authProvider = new AuthCodeMSALBrowserAuthenticationProvider(
   publicClientApplication,
   options
 );
+
+console.log("baz");
 
 export const graphClient = Client.initWithMiddleware({
   authProvider,
