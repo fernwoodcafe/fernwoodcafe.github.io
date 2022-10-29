cadf10bea2e334cc0c99f53c5b1f26f4e8e82ab7
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-10-29 09:13:42 -0700

    Increase code coverage

--- apps/fernwood-costing/src/packages/domain/__tests__/availableUnitConversions.test.ts
+++ apps/fernwood-costing/src/packages/domain/__tests__/availableUnitConversions.test.ts
@@ -1,9 +1,26 @@
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { availableUnitConversions } from "../services";
 
 describe("when processing valid inputs", () => {
   it("does not throw", () => {
     // Act
     availableUnitConversions("gram");
+  });
+
+  [
+    {
+      fromUnit: "gram" as const,
+      expectedToUnits: ["pound", "kilogram", "gram"],
+    },
+  ].forEach(({ fromUnit, expectedToUnits }) => {
+    it("produces expected conversions", () => {
+      // Arrange
+
+      // Act
+      const toUnits = availableUnitConversions(fromUnit);
+
+      // Assert
+      expect(toUnits.sort()).toEqual(expectedToUnits.sort());
+    });
   });
 });
