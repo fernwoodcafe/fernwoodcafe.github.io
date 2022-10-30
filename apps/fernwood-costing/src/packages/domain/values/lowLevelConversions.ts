9bd027ab41ae639ba71bcb6269f43fe1e04ea140
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-10-30 12:36:49 -0700

    Factor out lowLevelConversions

---
+++ apps/fernwood-costing/src/packages/domain/values/lowLevelConversions.ts
@@ -0,0 +1,30 @@
+import type { UnitOfMeasure } from "../types/UnitOfMeasure";
+
+export type UnitConversionTuple = [UnitOfMeasure, UnitOfMeasure, number];
+
+/**
+ * For all volumns, use ml as the smallest base unit.
+ * For all masses, use grams as the smallest base unit.
+ * Then we derive high-level conversions from lower-level ones.
+ */
+const lowLevelConversionsFromSmallerToBigger: UnitConversionTuple[] = [
+  // Low Level Mass Conversions
+  ["gram", "kilogram", 1000],
+  ["gram", "ounce-mass", 28.346],
+  ["gram", "pound", 453.5924],
+  // Low Level Volume Conversions
+  ["millilitre", "cup-metric", 250],
+  ["millilitre", "litre", 1000],
+  ["millilitre", "ounce-fluid-us", 29.57],
+];
+
+const lowLevelConversionsFromBiggerToSmaller: UnitConversionTuple[] =
+  lowLevelConversionsFromSmallerToBigger.map(([from, to, conversion]) => {
+    const inverseConversion = 1 / conversion;
+    return [to, from, inverseConversion];
+  });
+
+export const lowLevelConversions = [
+  ...lowLevelConversionsFromSmallerToBigger,
+  ...lowLevelConversionsFromBiggerToSmaller,
+] as const;
--- apps/fernwood-costing/src/packages/domain/values/unitConversions.ts
+++ apps/fernwood-costing/src/packages/domain/values/unitConversions.ts
@@ -1,36 +1,7 @@
-import type {
-  UnitConversion,
-  UnitConversionTuple,
-} from "../types/UnitConversion";
+import type { UnitConversion } from "../types/UnitConversion";
 import { deriveHighLevelConversions } from "./deriveHighLevelConversions";
+import { lowLevelConversions } from "./lowLevelConversions";
 import { unitsOfMeasureMass, unitsOfMeasureVolume } from "./unitsOfMeasure";
-
-/**
- * For all volumns, use ml as the smallest base unit.
- * For all masses, use grams as the smallest base unit.
- * Then we derive high-level conversions from lower-level ones.
- */
-const lowLevelConversionsFromSmallerToBigger: UnitConversionTuple[] = [
-  // Low Level Mass Conversions
-  ["gram", "kilogram", 1000],
-  ["gram", "ounce-mass", 28.346],
-  ["gram", "pound", 453.5924],
-  // Low Level Volume Conversions
-  ["millilitre", "cup-metric", 250],
-  ["millilitre", "litre", 1000],
-  ["millilitre", "ounce-fluid-us", 29.57],
-];
-
-const lowLevelConversionsFromBiggerToSmaller: UnitConversionTuple[] =
-  lowLevelConversionsFromSmallerToBigger.map(([from, to, conversion]) => {
-    const inverseConversion = 1 / conversion;
-    return [to, from, inverseConversion];
-  });
-
-const lowLevelConversions = [
-  ...lowLevelConversionsFromSmallerToBigger,
-  ...lowLevelConversionsFromBiggerToSmaller,
-] as const;
 
 const derivedMassConversions: UnitConversion[] = deriveHighLevelConversions(
   lowLevelConversions,
