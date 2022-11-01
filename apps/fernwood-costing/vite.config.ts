49c4a71c2a4a542b2960791167ea0196336fbf27
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-10-31 18:51:22 -0700

    wip: Log in using cypress

--- apps/fernwood-costing/vite.config.ts
+++ apps/fernwood-costing/vite.config.ts
@@ -6,6 +6,11 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import { env } from "node:process";
 import { defineConfig } from "vite";
 
+// Ask vite to open using localhost instead of 127.0.0.1.
+// See https://vitejs.dev/config/server-options.html#server-host
+import dns from "dns";
+dns.setDefaultResultOrder("verbatim");
+
 // https://vitejs.dev/config/
 export default defineConfig({
   mode: "development",
