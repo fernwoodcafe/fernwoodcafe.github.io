3d08375ee9ec9053230d452ff762193e49d89617
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-11-01 07:45:00 -0700

    Test page load content on supplies

--- apps/fernwood-costing/cypress/e2e/supplies.cy.ts
+++ apps/fernwood-costing/cypress/e2e/supplies.cy.ts
@@ -1,5 +1,31 @@
 describe("supplies", () => {
-  it("passes", () => {
+  before(() => {
+    // Act
     cy.visit("/#/supplies");
+  });
+
+  it("has button 'New Supply'", () => {
+    // Assert
+    cy.get('[type="button"]').should("have.value", "New Supply");
+  });
+
+  [
+    "Supply Name",
+    "Supplier Name",
+    "Supply Type",
+    "Supply Units",
+    "Purchase Quantity",
+    "Purchase Price before Tax",
+    "Percent Waste",
+    "Has PST",
+    "Supply Notes",
+    "Supply Link",
+    "Unit Cost",
+    "Actions",
+  ].forEach((headerText) => {
+    it(`has table header '${headerText}'`, () => {
+      // Assert
+      cy.contains(".ag-header-cell-text", headerText);
+    });
   });
 });
