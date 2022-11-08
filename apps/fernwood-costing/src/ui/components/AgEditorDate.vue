4486c0f271cef1674cef361fe4b226fca048bb70
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-11-07 21:04:25 -0800

    TODO on using ISO 8601 dates

--- apps/fernwood-costing/src/ui/components/AgEditorDate.vue
+++ apps/fernwood-costing/src/ui/components/AgEditorDate.vue
@@ -8,24 +8,13 @@ type Props = {
   params: ICellEditorParams;
 };
 
+// TODO [ui] Make the chosen date in human readable format instead of YYYY-MM-DD.
+// TODO [architecture] Pass around dates as ISO 8601 strings instead of Date objects.
+// TODO [architecture] Introduce an custom IsoDateString type to store that value.
+// Why? This avoids the problem of de/serialization conflicts as we pass around dates.
+// We can always count on the string being an ISO 8601 string instead of being serialized
+// in some way that we do not expect.
 export default defineComponent({
-  mounted() {
-    this.$nextTick(() => {
-      if (this.$refs.input instanceof HTMLInputElement) {
-        // TODO Make the chosen date in human readable format instead of YYYY-MM-DD.
-        this.$refs.input.focus();
-        this.$refs.input.select();
-
-        // TODO Fix type check error here.
-        // > Property 'showPicker' does not exist on type 'HTMLInputElement'.
-        // TODO Fix cypress test error here.
-        // > Failed to execute 'showPicker' on 'HTMLInputElement':
-        // > HTMLInputElement:: showPicker() requires a user gesture.
-        // The showPicker seems to require a
-        this.$refs.input.click();
-      }
-    });
-  },
   setup(props: Props) {
     const date = ref(props.params.value);
     return {
