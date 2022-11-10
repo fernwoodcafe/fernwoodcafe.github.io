6bfdaad68f98fde45208225a02b7c270a8a413ea
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-11-09 20:16:01 -0800

    wip: Cypress test sort order

--- apps/fernwood-costing/cypress/e2e/supplies-behavior.cy.ts
+++ apps/fernwood-costing/cypress/e2e/supplies-behavior.cy.ts
@@ -1,5 +1,8 @@
 const suppliesToCreate = 5;
 
+const toInnerText = (cells: JQuery<HTMLElement>) =>
+  cells.toArray().map((c) => c.innerText);
+
 describe("supplies behavior - creates supplies", () => {
   before(() => {
     // Act
@@ -25,5 +28,29 @@ describe("supplies behavior - creates supplies", () => {
     for (let i = 0; i < suppliesToCreate; ++i) {
       cy.contains(`${i}_supply`);
     }
+  });
+
+  // TODO Test that the table sorts properly.
+  // See https://www.cypress.io/blog/2020/07/27/sorting-the-table/
+  it("sorts by suppy name", () => {
+    cy.contains(".ag-header-cell-label", "Supply Name")
+      // The first click does not change the order,
+      // because we added the supplies in order.
+      .click()
+      .click()
+      .then(() => {
+        cy.get("[col-id=supplyName].ag-cell")
+          .then(toInnerText)
+          // TODO [bug] For some reason, even the the items appear reversed on
+          // the user interface, cypress.get fetches them in the non reversed
+          // order. Wassup with that?
+          .then((cellsInnerText) => {
+            cy.log(cellsInnerText.join(","));
+            const reversed = cellsInnerText.slice().reverse();
+            expect(cellsInnerText, "cells are reversed").to.deep.equal(
+              reversed
+            );
+          });
+      });
   });
 });
