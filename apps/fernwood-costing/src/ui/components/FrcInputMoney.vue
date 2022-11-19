b299a63f9a5521c5a0989036a60cdb759efa019c
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-11-18 19:54:47 -0800

    Make label for input

--- apps/fernwood-costing/src/ui/components/FrcInputMoney.vue
+++ apps/fernwood-costing/src/ui/components/FrcInputMoney.vue
@@ -1,7 +1,7 @@
 <template>
   <fieldset>
-    <label v-if="{ label }">{{ label }}</label>
-    <input v-model.lazy.number="moneyRef" type="number" />
+    <label v-if="{ label }" for="{{foo}}">{{ label }}</label>
+    <input id="{{foo}}" v-model.lazy.number="moneyRef" type="number" />
   </fieldset>
 </template>
 <script setup lang="ts">
@@ -17,6 +17,11 @@ type Props = {
 
 const emit = defineEmits<Emits>();
 const props = defineProps<Props>();
+
+// See https://eslint.vuejs.org/user-guide/#the-variables-used-in-the-template-are-warned-by-no-unused-vars-rule
+// See also our local file: .eslintrc.cjs
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const inputId = ref(props.label);
 
 const moneyRef = ref(props.value);
 
