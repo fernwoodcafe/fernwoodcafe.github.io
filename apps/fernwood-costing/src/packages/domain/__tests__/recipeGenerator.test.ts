3608e6156372b447c67b66d0f9ce978e67f67699
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-12-24 17:26:00 -0800

    Introduce recipeGenerator

--- apps/fernwood-costing/src/packages/domain/__tests__/recipeGenerator.test.ts
+++ apps/fernwood-costing/src/packages/domain/__tests__/recipeGenerator.test.ts
@@ -7,18 +7,28 @@ describe("recipeGenerator", () => {
     const options: RecipeOptions = {
       availableSizesInOunces: [8, 12, 16],
       availableExpressoShots: [2, 4, 6],
+      availableMilkAlternatives: [
+        "dairy_one_percent",
+        "dairy_3_percent",
+        "dairy_10_percent",
+        "oat",
+        "almond",
+      ],
     };
 
     // Act
     const recipes = recipeGenerator(options);
 
-    // Assert
-    expect(recipes).to.deep.include.members([{
-      size: 8,
-      shots: 2
-    }]);
-
     // Dump
     console.table(recipes);
+
+    // Assert
+    expect(recipes).to.deep.include.members([
+      {
+        size: 8,
+        shots: 2,
+        milk: 'oat'
+      },
+    ]);
   });
 });
--- apps/fernwood-costing/src/packages/domain/recipeGenerator/index.ts
+++ apps/fernwood-costing/src/packages/domain/recipeGenerator/index.ts
@@ -1,9 +1,17 @@
-export type AvailableSizesInOunces = readonly (8 | 12 | 16)[];
-export type AvailableEspressoShots = readonly (2 | 4 | 6)[];
+type AvailableSizesInOunces = readonly (8 | 12 | 16)[];
+type AvailableEspressoShots = readonly (2 | 4 | 6)[];
+type AvailableMilkAlternatives = readonly (
+  | "dairy_one_percent"
+  | "dairy_3_percent"
+  | "dairy_10_percent"
+  | "oat"
+  | "almond"
+)[];
 
 export type RecipeOptions = {
   availableSizesInOunces: AvailableSizesInOunces;
   availableExpressoShots: AvailableEspressoShots;
+  availableMilkAlternatives: AvailableMilkAlternatives;
 };
 
 export default (options: RecipeOptions) => {
@@ -11,13 +19,14 @@ export default (options: RecipeOptions) => {
     .map((size) => ({
       size,
     }))
-    .map((withSize) =>
-      options.availableExpressoShots.map((shots) =>
-        ({
-          ...withSize,
-          shots,
-        })
-      )
+    .flatMap((accumulator) =>
+      options.availableExpressoShots.map((shots) => ({
+        ...accumulator,
+        shots,
+      }))
     )
-    .flat();
+    .flatMap((accumulator) => options.availableMilkAlternatives.map((milk) => ({
+      ...accumulator,
+      milk
+    })));
 };
