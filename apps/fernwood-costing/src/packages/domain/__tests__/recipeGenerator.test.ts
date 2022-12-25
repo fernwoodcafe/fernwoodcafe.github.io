d94ae2e7aed6e27a4385b9dbb68f2236f2e57490
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-12-24 18:35:21 -0800

    Add packaging cost

--- apps/fernwood-costing/src/packages/domain/__tests__/recipeGenerator.test.ts
+++ apps/fernwood-costing/src/packages/domain/__tests__/recipeGenerator.test.ts
@@ -3,29 +3,14 @@ import recipeGenerator, { type CustomerOptions } from "../recipeGenerator";
 
 
 
-
-
-
-
-
 describe("recipeGenerator", () => {
   it("outputs expected recipes", () => {
     // Arranage
     const options: CustomerOptions = {
-      availableSizesInOunces: [8, 12, 16],
-      availableExpressoShots: [2, 4, 6],
-      availableMilkAlternatives: [
-        "dairy_one_percent",
-        "dairy_3_percent",
-        "dairy_10_percent",
-        "oat",
-        "almond",
-      ],
-      availableCups: [
-        'for_here',
-        'own_cup',
-        'to_go'
-      ],
+      availableSizesInOunces: [12],
+      availableExpressoShots: [2],
+      availableMilkAlternatives: ["dairy_3_percent"],
+      availableCups: ["for_here", "own_cup", "to_go"],
     };
 
     // Act
@@ -38,10 +23,10 @@ describe("recipeGenerator", () => {
     expect(recipes).to.deep.include.members([
       {
         // 12 ounce 2 shot oat milk latte to go.
-        size: 12,
+        cupSizeOunces: 12,
         espressoShots: 2,
-        milkAlternative: 'dairy_3_percent',
-        cup: 'to_go',
+        milkAlternative: "dairy_3_percent",
+        cupKind: "to_go",
         // espresso cost
         espressoGrams: 18.5,
         espressoFluidOunces: 1.5,
@@ -51,7 +36,7 @@ describe("recipeGenerator", () => {
         // milkColdOunces: 8.5,
         // milkCostDollars: 0.42,
         // packaging cost
-        // packagingCostDollars: 0.38
+        packagingCostDollars: 0.38
       },
     ]);
   });
--- apps/fernwood-costing/src/packages/domain/recipeGenerator/index.ts
+++ apps/fernwood-costing/src/packages/domain/recipeGenerator/index.ts
@@ -1,5 +1,5 @@
-type AvailableCups = "for_here" | "to_go" | "own_cup";
-type AvailableSizesInOunces = 8 | 12 | 16;
+type AvailableCupKinds = "for_here" | "to_go" | "own_cup";
+type AvailableCupSizesInOunces = 8 | 12 | 16;
 type AvailableEspressoShots = 2 | 4 | 6;
 type AvailableMilkAlternatives =
   | "dairy_one_percent"
@@ -9,42 +9,67 @@ type AvailableMilkAlternatives =
   | "almond";
 
 export type CustomerOptions = {
-  availableSizesInOunces: readonly AvailableSizesInOunces[];
+  availableSizesInOunces: readonly AvailableCupSizesInOunces[];
   availableExpressoShots: readonly AvailableEspressoShots[];
   availableMilkAlternatives: readonly AvailableMilkAlternatives[];
-  availableCups: readonly AvailableCups[];
+  availableCups: readonly AvailableCupKinds[];
 };
 
 export type RecipeVariant = {
-  size: AvailableSizesInOunces;
-  cup: AvailableCups;
+  cupSizeOunces: AvailableCupSizesInOunces;
+  cupKind: AvailableCupKinds;
   espressoShots: AvailableEspressoShots;
   milkAlternative: AvailableMilkAlternatives;
 };
 
+const packagingCostFor = (
+  options: Pick<RecipeVariant, "cupKind" | "cupSizeOunces">
+) => {
+  if (options.cupKind !== "to_go") {
+    return 0;
+  }
+
+  // cup + lid + sleeve
+  switch (options.cupSizeOunces) {
+    case 8:
+      // Note: We currently do not have 8 ounce sleeves;
+      // people tend not to ask for them.
+      return Math.round((0.13 + 0.12 + 0.00) * 100) / 100;
+    case 12:
+      return Math.round((0.17 + 0.13 + 0.08)* 100) / 100;
+    case 16:
+      return Math.round((0.19 + 0.13 + 0.08) * 100) / 100;
+  }
+};
+
 export default (options: CustomerOptions): RecipeVariant[] =>
   options.availableSizesInOunces
-    .map((size) => ({
-      size,
+    .map((cupSizeOunces) => ({
+      cupSizeOunces,
     }))
-    .flatMap((accumulator) =>
-      options.availableCups.map((cup) => ({
-        ...accumulator,
-        cup,
+    .flatMap((recipe) =>
+      options.availableCups.map((cupKind) => ({
+        ...recipe,
+        cupKind,
       }))
     )
-    .flatMap((accumulator) =>
+    .flatMap((recipe) =>
       options.availableExpressoShots.map((espressoShots) => ({
-        ...accumulator,
+        ...recipe,
         espressoShots,
-        espressoGrams: (18.5 * (espressoShots / 2)),
-        espressoFluidOunces: (1.5 * (espressoShots / 2)),
-        espressoCostDollars: (0.57 * (espressoShots / 2)),
       }))
     )
-    .flatMap((accumulator) =>
+    .flatMap((recipe) =>
       options.availableMilkAlternatives.map((milkAlternative) => ({
-        ...accumulator,
+        ...recipe,
         milkAlternative,
       }))
-    );
+    )
+    .map((recipe) => ({
+      ...recipe,
+      // TODO [refactor] Clean up these formulas.
+      espressoGrams: 18.5 * (recipe.espressoShots / 2),
+      espressoFluidOunces: 1.5 * (recipe.espressoShots / 2),
+      espressoCostDollars: 0.57 * (recipe.espressoShots / 2),
+      packagingCostDollars: packagingCostFor(recipe),
+    }));
