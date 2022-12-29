4cfa0f0b5b37a16f59efa0594f470d124da4b6a5
Shaun Luttin <shaun.luttin@mediavalet.com>
2022-12-28 20:19:22 -0800

    Add TODO

--- apps/fernwood-costing/_entropy/frc-costing-2023/src/businessStrategies.ts
+++ apps/fernwood-costing/_entropy/frc-costing-2023/src/businessStrategies.ts
@@ -5,6 +5,7 @@ const businessStrategy = {
   grossRevenueCurrency: 325_000,
   cogsPercent: 0.3,
   postCogsLabourCostPercent: 0.65,
+  // TODO [must-have] Model the _total_ for overhead like rent, electricity...
 };
 
 export default [businessStrategy];
